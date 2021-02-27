31ce2bffc0d29a5740cfa0fd23834db1be0996a1
Leonardo <leojgo@users.noreply.github.com>
2021-02-27 16:36:42 -0500

    Refactoring configuration code; Fix connection string bug in production

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
+import os
 from flask import Flask
-from config import Config
+from config import Config, ProductionConfig
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-app.config.from_object(Config)
+if os.environ.get('FLASK_ENV') == 'production':
+    app.config.from_object(ProductionConfig)
+else:
+    app.config.from_object(Config)
+
 db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 
--- config.py
+++ config.py
@@ -1,13 +1,23 @@
 import os
+import urllib
 
 basedir = os.path.abspath(os.path.dirname(__file__))
+database_url = os.environ.get('DATABASE_URL')
 
 class Config(object):
+    """ Base configuration """
     # Database configuration
-    #  TODO: later to change to a file => 'sqlite:///' + os.path.join(basedir, 'calicotdb.db')+"?mode=memory&cache=shared"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = database_url or \
         'postgresql:///calicotdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Cryptographic key
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'voce-nunca-vai-adivinhar-esta-senha'
+
+class ProductionConfig(Config):
+    if database_url:
+        params = urllib.parse.quote_plus(database_url)
+        SQLALCHEMY_DATABASE_URI = f"mssql+pyodbc:///?odbc_connect={params}"
+    else:
+        if os.environ.get('FLASK_ENV') == 'production':
+            raise ValueError("Environment variable not set: DATABASE_URL")
