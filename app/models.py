a3029efcd94c33d6507ea90cf72cfa6c8174e5c6
Leonardo <leojgo@users.noreply.github.com>
2021-03-08 23:19:18 -0500

    Fix column size for product images

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class Product(db.Model):
     name = db.Column(db.String(64), index=True, unique=True)
     description = db.Column(db.String(200))
     price = db.Column(db.Numeric(10,2))
-    photo_url = db.Column(db.String(50))
+    photo_url = db.Column(db.String(250))
 
     def __repr__(self):
         return f"Product('{self.name}', '{self.price}')"
